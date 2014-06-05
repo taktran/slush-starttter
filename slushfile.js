e2872437e64a3acf2b4f0033dad199df585d2fc9
Tak Tran <contact@tutaktran.com>
2014-06-05 23:43:10 +0100

    Clean up slush file.

--- slushfile.js
+++ slushfile.js
@@ -8,48 +8,53 @@
 
 'use strict';
 
-var gulp = require('gulp'),
-    install = require('gulp-install'),
-    conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
-    rename = require('gulp-rename'),
-    _ = require('underscore.string'),
-    inquirer = require('inquirer');
+var gulp = require('gulp');
+var install = require('gulp-install');
+var conflict = require('gulp-conflict');
+var template = require('gulp-template');
+var rename = require('gulp-rename');
+var _str = require('underscore.string');
+var inquirer = require('inquirer');
 
 gulp.task('default', function (done) {
-    var prompts = [{
-        type: 'input',
-        name: 'appName',
-        message: 'What is the name of your generator?',
-        default: gulp.args.join(' ')
-    }, {
-        type: 'input',
-        name: 'appDescription',
-        message: 'What is the description for your generator?'
-    }, {
-        type: 'confirm',
-        name: 'moveon',
-        message: 'Continue?'
-    }];
-    //Ask
-    inquirer.prompt(prompts,
-        function (answers) {
-            if (!answers.moveon) {
-                return done();
-            }
-            answers.appNameSlug = _.slugify(answers.appName);
-            gulp.src(__dirname + '/templates/**')
-                .pipe(template(answers))
-                .pipe(rename(function (file) {
-                    if (file.basename[0] === '_') {
-                        file.basename = '.' + file.basename.slice(1);
-                    }
-                }))
-                .pipe(conflict('./'))
-                .pipe(gulp.dest('./'))
-                .pipe(install())
-                .on('end', function () {
-                    done();
-                });
+  var prompts = [
+    {
+      type: 'input',
+      name: 'appName',
+      message: 'What is the name of your generator?',
+      default: gulp.args.join(' ')
+    },
+    {
+      type: 'input',
+      name: 'appDescription',
+      message: 'What is the description for your generator?'
+    },
+    {
+      type: 'confirm',
+      name: 'moveon',
+      message: 'Continue?'
+    }
+  ];
+
+  //Ask
+  inquirer.prompt(prompts,
+    function (answers) {
+      if (!answers.moveon) {
+        return done();
+      }
+      answers.appNameSlug = _str.slugify(answers.appName);
+      gulp.src(__dirname + '/templates/**')
+        .pipe(template(answers))
+        .pipe(rename(function (file) {
+          if (file.basename[0] === '_') {
+            file.basename = '.' + file.basename.slice(1);
+          }
+        }))
+        .pipe(conflict('./'))
+        .pipe(gulp.dest('./'))
+        .pipe(install())
+        .on('end', function () {
+          done();
         });
+    });
 });
